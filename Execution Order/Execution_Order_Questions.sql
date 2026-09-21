6545b59360d237194a7ad8d70c76e7476d7d531d
Swornim67 <regmiswornim2061@gmail.com>
2026-09-21 19:28:20 +0545

    Done TIll Question No 3.

---
+++ Execution Order/Execution_Order_Questions.sql
@@ -0,0 +1,36 @@
+-- DATABASE USING 
+use sql_execution_order;
+
+-- TABLES USING 
+select * from customers;
+select * from orders;
+
+-- Question 1
+
+-- Find the total revenue by customer.
+select 
+c.customer_name,sum(o.order_amount) as `TOTAL REVENUE`
+from customers c
+join orders o 
+on c.customer_id=o.customer_id
+group by c.customer_name;
+
+-- Question 2
+-- Find the total delivered revenue by customer.
+select 
+c.customer_name,sum(o.order_amount) as `TOTAL DELIVERED REVENUE`
+from customers c 
+join orders o 
+on c.customer_id=o.customer_id
+where o.order_status='Delivered'
+group by c.customer_name;
+
+-- Question 3
+-- Find customers whose delivered revenue is more than 50,000.
+select 
+c.customer_name,sum(o.order_amount) as `TOTAL REVENUE`
+from customers c 
+join orders o 
+on c.customer_id=o.customer_id
+group by c.customer_name
+having sum(o.order_amount)>50000;
